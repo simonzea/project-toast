ec1e92a495c6a9dec387a985b27bd0f922e64ba1
Simon Zea <szeag@unal.edu.co>
2024-05-12 18:15:24 -0500

    Exercise 1: Wiring up form controls

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,8 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [variantOption, setVariantOption] = React.useState(VARIANT_OPTIONS[0]);
+  const [message, setMessage] = React.useState('');
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +26,16 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+             id="message"
+            className={styles.messageInput}
+            value={message}
+            onChange={event => {
+              setMessage(
+                event.target.value
+              );
+            }}
+            />
           </div>
         </div>
 
@@ -33,15 +44,21 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
+            {VARIANT_OPTIONS.map( variant => (
+            <label key={variant} htmlFor="variant-notice">
               <input
                 id="variant-notice"
                 type="radio"
                 name="variant"
-                value="notice"
+                value={variant}
+                checked={variantOption === variant}
+                onChange={event => {
+                  setVariantOption(event.target.value)
+            }}
               />
-              notice
-            </label>
+              {variant}
+            </label>) )}
+
 
             {/* TODO Other Variant radio buttons here */}
           </div>
